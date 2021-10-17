56fece61d293a79bde2c75faeefd0d7be25f0d3b
Wilson Shrestha <wilsonshresthaapps@gmail.com>
2021-10-17 17:05:00 +0545

    api

--- src/test/java/com/noob/coder/MarsRoverApiTest.java
+++ src/test/java/com/noob/coder/MarsRoverApiTest.java
@@ -12,7 +12,7 @@ public class MarsRoverApiTest {
 	public void smallTest() {
 		RestTemplate rt = new RestTemplate();
 		ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity(
-				"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=4xGnh9vzNw9G2kvBP8Q0Gt61IlXDk9C5JKhWmnq0",
+				"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=hidden",
 				MarsRoverApiResponse.class);
 		System.out.println(response.getBody());
 	}
