231425418f399029d8f8e1aba946c40937a9db09
WilcyWilson <wilsonshresthaapps@gmail.com>
2021-10-25 22:09:35 +0545

    Thymeleaf Databinding th:field

--- src/main/java/com/noob/coder/web/HomeController.java
+++ src/main/java/com/noob/coder/web/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.noob.coder.dto.HomeDto;
 import com.noob.coder.response.MarsRoverApiResponse;
 import com.noob.coder.service.MarsRoverApiService;
 
@@ -17,16 +17,16 @@ public class HomeController {
 	private MarsRoverApiService roverService;
 
 	@GetMapping("/")
-	public String getHomeView(ModelMap model, @RequestParam(required = false) String marsApiRoverData,
-			@RequestParam(required = false) Integer marsSol,@RequestParam(required = false) Boolean defaultCheck1) {
-		if (ObjectUtils.isEmpty(marsApiRoverData)) {
-			marsApiRoverData = "opportunity";
+	public String getHomeView(ModelMap model, HomeDto homeDto) {
+		if (ObjectUtils.isEmpty(homeDto.getMarsApiRoverData())) {
+			homeDto.setMarsApiRoverData("opportunity");
 		}
-		if (ObjectUtils.isEmpty(marsSol)) {
-			marsSol = 1;
+		if (ObjectUtils.isEmpty(homeDto.getMarsSol())) {
+			homeDto.setMarsSol(1);
 		}
-		MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData, marsSol);
+		MarsRoverApiResponse roverData = roverService.getRoverData(homeDto.getMarsApiRoverData(), homeDto.getMarsSol());
 		model.put("roverData", roverData);
+		model.put("homeDto", homeDto);
 		return "index";
 	}
 }
